873e45f4e9e7d7bae0a8d87e475fd2c262f545ef
Kunal-Paste <pastekunal6@gamail.com>
2025-10-24 10:45:28 +0530

    learned about static

---
+++ OOPS2.ts
@@ -0,0 +1,7 @@
+class Shery {
+    static  version = 1.0;
+}
+
+//here without making and instance of class shery we can access version because we have added static 
+
+// Shery.version;  // like this 
